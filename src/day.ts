c74b1b12c16b86a1ab5462c772000d1bcb8a9c2e
Paulus Esterhazy <pesterhazy@gmail.com>
2022-12-03 09:11:09 +0100

    WIP

--- src/day.ts
+++ src/day.ts
@@ -1,3 +1,25 @@
+import * as tools from "../src/tools";
+import * as assert from "assert";
+
 export function solve(s: string) {
-  return -777;
+  let lines = tools.asLines(s);
+  let total = 0;
+
+  for (let line of lines) {
+    let a = new Set(Array.from(line.slice(0, line.length / 2)));
+    let b = new Set(Array.from(line.slice(line.length / 2)));
+
+    let found = undefined;
+    for (let x of b) if (a.has(x)) found = x;
+    assert(found !== undefined);
+
+    let n;
+    if (found.match(/[a-z]/)) n = found.charCodeAt(0) - "a".charCodeAt(0) + 1;
+    else if (found.match(/[A-Z]/))
+      n = found.charCodeAt(0) - "A".charCodeAt(0) + 27;
+    else assert(false);
+
+    total += n;
+  }
+  return total;
 }
--- test/day.test.ts
+++ test/day.test.ts
@@ -1,11 +1,22 @@
 import { it } from "bun:test";
 import { deepStrictEqual as eq } from "assert";
-import * as day from "../src/day";
 import * as tools from "../src/tools";
+import * as day from "../src/day";
 
 it("", () => {
   let ii = `
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
 `;
   let r = day.solve(ii);
-  eq(r, -777);
+  eq(r, 157);
+});
+
+it("", () => {
+  let r = day.solve(tools.inputFor(3));
+  eq(r, 157);
 });
