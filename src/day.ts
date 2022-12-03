f84b21990531085b7214ab6564d638f33695ac40
Paulus Esterhazy <pesterhazy@gmail.com>
2022-12-03 08:50:44 +0100

    WIP

---
+++ src/day.ts
@@ -0,0 +1,3 @@
+export function solve(s: string) {
+  return -777;
+}
---
+++ test/day.test.ts
@@ -0,0 +1,11 @@
+import { it } from "bun:test";
+import { deepStrictEqual as eq } from "assert";
+import * as day from "../src/day";
+import * as tools from "../src/tools";
+
+it("", () => {
+  let ii = `
+`;
+  let r = day.solve(ii);
+  eq(r, -777);
+});
