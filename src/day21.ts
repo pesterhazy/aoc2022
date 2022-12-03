a9e75778d1cba88968e065b522a510e81493397c
Paulus Esterhazy <pesterhazy@gmail.com>
2022-12-03 16:44:37 +0100

    WIP

---
+++ src/day21.ts
@@ -0,0 +1,5 @@
+import * as tools from "../src/tools";
+import * as assert from "assert";
+import * as setutils from "ts-set-utils";
+
+export function solve(s: string) {}
---
+++ test/day24.test.ts
@@ -0,0 +1,12 @@
+import { it } from "bun:test";
+import { deepStrictEqual as eq } from "assert";
+import * as tools from "../src/tools";
+import * as day from "../src/day24";
+
+let demo = `
+`;
+
+it("", () => {
+  let r = day.solve(demo);
+  eq(r, undefined);
+});
