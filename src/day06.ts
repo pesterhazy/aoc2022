624966d6cc6f4d8060fc462ba5f0c976d5aa6622
Paulus Esterhazy <pesterhazy@gmail.com>
2022-12-06 19:57:10 +0100

    WIP

--- src/day06.ts
+++ src/day06.ts
@@ -2,4 +2,17 @@ import * as tools from "../src/tools";
 import * as assert from "assert";
 import * as setutils from "ts-set-utils";
 
-export function solve(s: string) {}
+export function solve(s: string) {
+  for (let i = 3; i < s.length - 1; i++) {
+    let seen = new Set();
+    let dupe = false;
+    for (let j = i - 3; j <= i; j++) {
+      if (seen.has(s[j])) {
+        dupe = true;
+        break;
+      }
+      seen.add(s[j]);
+    }
+    if (!dupe) return i + 1;
+  }
+}
--- test/day06.test.ts
+++ test/day06.test.ts
@@ -3,10 +3,19 @@ import { deepStrictEqual as eq } from "assert";
 import * as tools from "../src/tools";
 import * as day from "../src/day06";
 
-let demo = `
-`;
+let demo = `mjqjpqmgbljsphdztnvjfqwrcgsmlb`;
 
 it("", () => {
   let r = day.solve(demo);
-  eq(r, undefined);
+  eq(r, 7);
+});
+
+it("", () => {
+  let r = day.solve("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw");
+  eq(r, 11);
+});
+
+it("", () => {
+  let r = day.solve(tools.inputFor(6));
+  eq(r, 11);
 });
